1b29edad7ae3ad2796e41bc374624ec05a2eeace
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2025-10-06 14:13:54 +0300

    make desc

--- src/Components/ComponentsView.vue
+++ src/Components/ComponentsView.vue
@@ -23,63 +23,46 @@
             <div id="struktura">
                 <h2 class="border-b-1 mt-5 text-2xl">Struktūra</h2>
                 <div class="mt-10 w-full flex justify-center">
-                    <img src="@/assets/sequence.png" class=""></img>
+                    <img src="@/assets/compdiag.png" class=""></img>
                 </div>
 
                 <div class="pl-5 border-b-1 border-t-1 pt-5">
-                    <h3 class="text-xl">Actor (Lietotājs)</h3>
+                    <h3 class="text-xl">Komponente</h3>
                     <div class="flex gap-5 mb-3 mt-5  justify-start items-start" :class="smallScreen ? 'flex-wrap items-center justify-center' : ''">
-                        <div class="w-50">
-                            <img class="" src="@/assets/actor.png"></img>
+                        <div class="w-200">
+                            <img class="" src="@/assets/component.png"></img>
                         </div>
-                        <p class="w-auto">Actor (Lietotājs) ir diagrammas elements, kas pārstāv ārēju entītiju, piemēram, cilvēku vai sistēmu, kas mijiedarbojas ar sistēmu. Aktieris iniciē notikumus un saņem atbildes no sistēmas.</p>
+                        <p class="w-auto">Norāda sistēmas modulārās daļas, kurā atrodas funkcionalitātes. Komponentes var nūt programmatūras klases, klašu kopas vai apakšsistēmas.</p>
                     </div>
                 </div>
 
                 <div class="pl-5 border-b-1 border-t-1 pt-5">
-                    <h3 class="text-xl">Lifelines (Dziveslīnijas)</h3>
+                    <h3 class="text-xl">Interfeiss</h3>
                     <div class="flex gap-5 mb-3 mt-5  justify-start items-start" :class="smallScreen ? 'flex-wrap items-center justify-center' : ''">
                         <div class="w-50">
-                            <img class="" src="@/assets/lifelines2.png"></img>
+                            <img class="" src="@/assets/interface.png"></img>
                         </div>
-                        <p class="w-auto">Lifeline (Dzīveslīnija) ir nosaukts elements, kas attēlo atsevišķu dalībnieku sekvenču diagrammā. Citiem vārdiem, katra instance diagrammā tiek attēlota ar dzīveslīniju.</p>
+                        <p class="w-auto">Norāda darbības, kurā komponente piedāvā vai ir nepieciešama. Rinķi norāda interfeisus un pusapļi norāda nepieciešamību savienojumam ar interfeisu.</p>
                     </div>
                 </div>
 
                 <div class="pl-5 border-b-1 border-t-1 pt-5">
-                    <h3 class="text-xl">Messages (Ziņojumi)</h3>
+                    <h3 class="text-xl">Ports</h3>
                     <div class="flex gap-5 mb-3 mt-5  justify-start items-start" :class="smallScreen ? 'flex-wrap items-center justify-center' : ''">
                         <div class="w-80">
-                            <img class="" src="@/assets/messages.png"></img>
+                            <img class="" src="@/assets/port.png"></img>
                         </div>
-                        <p class="w-auto">Messages (Ziņojumi) ir Saziņa starp objektiem tiek attēlota ar ziņojumiem. Tie uz dzīveslīnijas parādās noteiktā secībā. Ziņojumi diagrammā tiek attēloti ar bultiņām. Dzīveslīnijas un ziņojumi veido sekvenču diagrammas pamatu. Kopumā ziņojumus var iedalīt vairākās kategorijās:</p>
+                        <p class="w-auto">
+                            Norāda punktu, kurā komponentei interfeisus piedāvā vai ir nepieciešama.
+                        </p>
                     </div>
                 </div>
 
-                <div class="pl-5 border-b-1 border-t-1 pt-5">
-                    <h3 class="text-xl">Synchronous messages (Sinhronie ziņojumi)</h3>
-                    <div class="flex gap-5 mb-3 mt-5  justify-start items-start" :class="smallScreen ? 'flex-wrap items-center justify-center' : ''">
-                        <div class="w-140">
-                            <img class="" src="@/assets/synchro.png"></img>
-                        </div>
-                        <p class="w-auto">Synchronous messages (Sinhronie ziņojumi) – Sinhrons ziņojums gaida atbildi, pirms mijiedarbība var turpināties. Sūtītājs aptur savu darbību, līdz saņēmējs ir pilnībā apstrādājis ziņojumu. Tātad izsaucējs turpina tikai tad, kad ir pārliecināts, ka saņēmējs ir izpildījis iepriekšējo ziņojumu un ir saņemta atbilde. Objektorientētajā programmēšanā liela daļa izsaukumu ir tieši sinhroni. Lai attēlotu sinhronu ziņojumu, izmanto bultiņu ar aizpildītu uzgali.</p>
-                    </div>
-                </div>
-
-                <div class="pl-5 border-b-1 border-t-1 pt-5">
-                    <h3 class="text-xl">Asynchronous messages (Asinhronie ziņojumi)</h3>
-                    <div class="flex gap-5 mb-3 mt-5  justify-start items-start" :class="smallScreen ? 'flex-wrap items-center justify-center' : ''">
-                        <div class="w-90">
-                            <img class="" src="@/assets/async.png"></img>
-                        </div>
-                        <p class="w-auto">Asinhronie ziņojumi (Asynchronous messages) – Asinhrons ziņojums negaida atbildi no saņēmēja. Mijiedarbība turpinās neatkarīgi no tā, vai saņēmējs jau ir apstrādājis iepriekšējo ziņojumu vai nē. Lai attēlotu asinhronu ziņojumu, izmanto bultiņu ar atvērtu (nepiepildītu) uzgali.</p>
-                    </div>
-                </div>
             </div>
 
             <div id="test" class="mt-5 border-1 p-5 rounded shadow-md">
                 <h2>Tests</h2>
-                <p class="mt-3">Šajā sadaļā būs tests par sekvenču UML diagrammām.</p>
+                <p class="mt-3">Šajā sadaļā būs tests par komponenšu UML diagrammām.</p>
                 <button @click="testStart = true" class="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 cursor-pointer">Sākt</button>
             </div>
 
